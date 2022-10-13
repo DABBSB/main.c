6a61144d5156450063eb9a597ebec4aff413befc
DABB <1433683384@qq.com>
2022-10-13 19:06:33 +0800

    添加栈zhan.c

---
+++ zhan.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node {
+    int data;
+    struct Node *top;
+} Node;
+
+Node *initStack() {
+    Node *node = (Node *) malloc(sizeof(Node));
+    node->data = 0;
+    node->top = NULL;
+    return node;
+}
+
+int isEmpty(Node *S) {
+    if (S->top == NULL || S->data == 0) {
+        return -1;
+    } else {
+        return 0;
+    }
+}
+
+void push(Node *S, int data) {
+    Node *node = (Node *) malloc(sizeof(Node));
+    node->data = data;
+    node->top = S->top;
+    S->top = node;
+    S->data++;
+}
+
+int pop(Node *S) {
+    if (isEmpty(S) == -1) {
+        return -1;
+    } else {
+        Node *node = S->top;
+        int data = node->data;
+        S->top = node->top;
+        free(node);
+        return data;
+    }
+}
+
+void print(Node *S) {
+    Node *node = S->top;
+    while (node) {
+        printf("%d", node->data);
+        node = node->top;
+    }
+    printf("NULL");
+}
+
+int main() {
+    Node *node = initStack();
+    push(node, 1);
+    push(node, 2);
+    push(node, 3);
+    push(node, 4);
+    pop(node);
+    pop(node);
+    print(node);
+
+}
---
+++ zhan.h
@@ -0,0 +1,14 @@
+//
+// Created by DABB on 2022/10/13.
+//
+
+#ifndef ZHIZHEN_C_ZHAN_H
+#define ZHIZHEN_C_ZHAN_H
+
+
+class zhan{
+
+};
+
+
+#endif //ZHIZHEN_C_ZHAN_H
